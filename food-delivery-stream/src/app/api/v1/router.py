9c254038971b1b9786060cef3933c73e7506a3f5
AdarshTiwari3 <a.tiwariadarsh99@gmail.com>
2026-03-29 15:35:29 +0530

    feat: add all the order related endpoints routing

--- food-delivery-stream/src/app/api/v1/router.py
+++ food-delivery-stream/src/app/api/v1/router.py
@@ -1,8 +1,9 @@
 from fastapi import APIRouter
 
-from src.app.api.v1.endpoints import health
+from src.app.api.v1.endpoints import health, orders
 
 api_v1_router = APIRouter()
 
 
-api_v1_router.include_router(health.router, prefix="/system")
+api_v1_router.include_router(health.router, prefix="/health", tags=["Health"])
+api_v1_router.include_router(orders.router, prefix="/orders", tags=["Orders"])
